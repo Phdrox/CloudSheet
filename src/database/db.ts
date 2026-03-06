718e5a33c7e4166e8dcf7c6f14db92e843cec590
Phdrox <phximenes2103@gmail.com>
2026-03-06 11:19:20 -0300

    error

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,3 +1,8 @@
-import "dotenv/config";
-import {drizzle} from "drizzle-orm/node-postgres";
-export const db=drizzle(process.env.DATABASE_URL!);
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
+import { config } from "dotenv";
+
+config({ path: ".env" }); // or .env.local
+
+const sql = neon(process.env.DATABASE_URL!);
+export const db = drizzle({ client: sql });
