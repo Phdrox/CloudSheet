b1686b799a8e79a83b027cd4ee67c555a6bb9478
Phdrox <phximenes2103@gmail.com>
2026-03-15 20:11:42 -0300

    cors

--- src/auth.ts
+++ src/auth.ts
@@ -4,28 +4,31 @@ import { db } from "./database/db.js"
 import { hash, verify } from "argon2"
 import * as schema from "./database/schemas.js"
 
-export const auth=betterAuth({
-    basePath:"/api/auth",
-    trustedOrigins:["https://cloud-sheet.vercel.app","http://localhost:3000"],
-    database:drizzleAdapter(db,{
-        provider:'pg',
-        schema
-    }),
-    advanced:{
-        database:{
-            generateId:false
-        }
-    },
-    emailAndPassword:{
-        enabled:true,
-        autoSignIn:true,
-        password:{
-            hash: async (password: string) => await hash(password),
-            verify: async ({ password, hash: storedHash }) => {
-                if (!storedHash) return false;
-                return await verify(storedHash, password);
-            }
-        }
-    },
+export const auth = betterAuth({
+  basePath: "/api/auth",
+
+  trustedOrigins: [
+    "http://localhost:3000",
+    "https://cloud-sheet.vercel.app"
+  ],
+
+  database: drizzleAdapter(db,{
+    provider:"pg",
+    schema
+  }),
+
+  emailAndPassword:{
+    enabled:true,
+    autoSignIn:true
+  },
+
+  cookies:{
+    sessionToken:{
+      attributes:{
+        sameSite:"none",
+        secure:true
+      }
+    }
+  }
 })
 
