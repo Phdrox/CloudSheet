7129f12a03072afac940e7e8dae5c3f7c926d896
Phdrox <phximenes2103@gmail.com>
2026-03-16 10:12:53 -0300

    auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,13 +8,13 @@ import { GoalServices } from './goal/goal.services.js';
 import { CategoriesServices } from './categories/categories.services.js';
 import { GoalController } from './goal/goal.controllers.js';
 import { CategoryController } from './categories/categories.controllers.js';
-
+import { AuthModule } from '@thallesp/nestjs-better-auth';
 import { AppController } from './app.controller.js';
 import { auth } from './auth.js';
 import { UserController } from './users/user.controllers.js';
 
 @Module({
-  imports: [ConfigModule.forRoot(),],
+  imports: [ConfigModule.forRoot(),AuthModule.forRoot({auth})],
   controllers: [AppController,AuthController,FlowsController,GoalController,CategoryController,UserController],
   providers: [UsersService,FlowsServices,GoalServices,CategoriesServices]
 })
