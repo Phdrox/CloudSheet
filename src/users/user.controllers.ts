2842ecac2d8aa9b663048b79f08b5fa23b1d6d7e
Phdrox <phximenes2103@gmail.com>
2026-03-17 10:30:36 -0300

    cors

--- src/users/user.controllers.ts
+++ src/users/user.controllers.ts
@@ -26,7 +26,6 @@ export class UserController{
     }
 
     @Post('/create/login')
-    @AllowAnonymous()
      async createUser(  
         @Body('email') email:string,
         @Body('password') password:string,
