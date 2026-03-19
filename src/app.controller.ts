5414787350b4c7ebb8986196da56981c7b91bdd8
Phdrox <phximenes2103@gmail.com>
2026-03-19 10:26:48 -0300

    auth

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,10 @@
 import { Controller,Get, Param } from "@nestjs/common";
 
-import { AllowAnonymous } from "@thallesp/nestjs-better-auth";
-
 
 @Controller('/')
 export class AppController{
 
     @Get("/")
-    @AllowAnonymous()
     async getAll(){
         return {helloWorld:":)"}
     }
