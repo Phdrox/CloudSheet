d98b5b8501e7c438792104f3ef3c316fd819cb6b
Phdrox <phximenes2103@gmail.com>
2026-03-24 10:31:45 -0300

    proxie

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    bodyParser: false
+    bodyParser: true
   });
 
   app.enableCors({
