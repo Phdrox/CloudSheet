b967b21ab1f67b8eee7c0b073e4e9a9024267074
Phdrox <phximenes2103@gmail.com>
2026-03-26 23:21:39 -0300

    authguard

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -36,9 +36,9 @@ export class AuthGuard implements CanActivate{
 
      const authorization=request.headers?.authorization;
 
-     if(authorization) return undefined;
+     if(!authorization) return undefined;
 
-     const [type,token]=authorization.split(' ');
+     const [type,token]=authorization.split(' ') ?? [];
      
      return type === 'Bearer' ? token : undefined;
    }
