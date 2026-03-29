6b1d77e9694d4eb90217751861cd6ee5291dd10e
Phdrox <phximenes2103@gmail.com>
2026-03-29 20:03:52 -0300

    pronto

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,11 +10,13 @@ import { CategoryController } from './categories/categories.controller.js';
 import { AppController } from './app.controller.js';
 import { AuthModule } from './auth/auth.module.js';
 import { UsersModule } from './users/users.module.js';
+import { BankController } from './banks/banks.controller.js';
+import { BankServices } from './banks/banks.service.js';
 
 
 @Module({
   imports: [ConfigModule.forRoot({isGlobal:true}), AuthModule, UsersModule,],
-  controllers: [AppController,FlowsController,GoalController,CategoryController,],
-  providers: [FlowsServices,GoalServices,CategoriesServices]
+  controllers: [AppController,FlowsController,GoalController,CategoryController,BankController],
+  providers: [FlowsServices,GoalServices,CategoriesServices,BankServices]
 })
 export class AppModule {}
