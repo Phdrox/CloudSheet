6da317cbd4305c3ef15629328596dd134bca8f97
Phdrox <phximenes2103@gmail.com>
2026-03-30 13:45:01 -0300

    banks

--- src/hook/pagination.ts
+++ src/hook/pagination.ts
@@ -75,7 +75,14 @@ export async function usePaginationIdBanks({page=1,search=""}:PaginationsType,ta
     const finalFilter = and(...[userFilter, searchFilter].filter(Boolean));
     const [totalResult, rows] = await Promise.all([
         db.select({ value: count() }).from(table).where(finalFilter),
-        db.select({flow:table,bank:{name:banks.name,compeCode:banks.compeCode}})
+        db.select({flow:{
+            id: table.id,
+            name: table.name,
+            type: table.type,
+            payment: table.payment,
+            price: table.price,
+            date: table.date},
+            bank:{name:banks.name,compeCode:banks.compeCode}})
         .from(table)
         .where(finalFilter)
         .leftJoin(banks,eq(table.id_name_banks,banks.id)).limit(limit).offset(offset)
