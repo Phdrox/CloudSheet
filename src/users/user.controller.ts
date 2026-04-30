fc34ec6e34dd3180d32bdb9f9dad49979eb038ea
Phdrox <phximenes2103@gmail.com>
2026-04-30 16:32:02 -0300

    project

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -5,31 +5,32 @@ import { AuthGuard } from "../auth/auth.guard.js";
 import { Roles } from "../roles/roles.decorator.js";
 import { Public } from "../roles/public.decorator.js";
 
+@UseGuards(AuthGuard)
 @Controller('/users')
 export class UserController{
 
     constructor( private readonly userService:UsersService){}
-    @UseGuards(AuthGuard)
+    
     @Roles('admin')
     @Get("")
     async getAll(){
         return await this.userService.getAllUsers()
     }
-    @UseGuards(AuthGuard)
+    
     @Roles('admin','user')
     @Get(":id")
     async getUser(@Param("id") id:UUID){
         return await this.userService.getUserById(id)
     }
 
-    @UseGuards(AuthGuard)
+    
     @Roles('admin')
     @Put(":id")
     async putUser(@Body() id:UUID, data:User,type:string){
         return await this.userService.putUserById(id,data,type)
     }
     
-    @UseGuards(AuthGuard)
+    
     @Roles('admin')
     @Delete(':id')
     async deleteUser(@Param() id:UUID){
